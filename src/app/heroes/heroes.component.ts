dccf62d1030ffeab8ca3e1373c090568d4fccab4
Carlos Menares <menaresdesarrollo@gmail.com>
2023-03-17 14:04:56 -0300

    se agrega servicios y se hacen los cambios respectivos en el componente

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+
+import { HeroService } from '../hero.service';
+
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+//import { HEROES } from './mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,16 +11,22 @@ import { HEROES } from './mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes!: Hero[];
   selectedHero: Hero | undefined;
   
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
   
